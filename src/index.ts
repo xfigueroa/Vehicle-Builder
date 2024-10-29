b5e416316d964b5849755d432be59acc977c0fb0
Xavier Figueroa <xfigueroa28@gmail.com>
2024-10-29 14:14:04 -0400

    Update Motorbike.ts and Truck.ts code now the Cli works

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import Cli from "./classes/Cli.js";
 const vehicles = [];
 
 // TODO: uncomment once trucks are implemented
-// const truck1 = new Truck(Cli.generateVin(),"red", "Ford", "F-150", 2021, 5000, 120, [], 10000);
+const truck1 = new Truck(Cli.generateVin(),"red", "Ford", "F-150", 2021, 5000, 120, [], 10000);
 
 // will use default wheels
 const car1 = new Car(
@@ -29,7 +29,7 @@ const car1 = new Car(
 
 // push vehicles to array
 // TODO: uncomment once trucks are implemented
-// vehicles.push(truck1);
+vehicles.push(truck1);
 vehicles.push(car1);
 // TODO: uncomment once motorbikes are implemented
 // vehicles.push(motorbike1);
