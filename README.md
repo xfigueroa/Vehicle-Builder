95562b7ef0d21536bdcb5185475401f540569997
Xavier Figueroa <xfigueroa28@gmail.com>
2024-10-29 23:37:30 -0400

    Update README.md file and linked test video

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# Vehicle-Builder
+# Vehicle Builder
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+## Description
+
+This command-line application allows you to manage various types of vehicles, including Cars, Trucks, and Motorbikes. You can create vehicles, perform actions on them, and manage towing capabilities for trucks.
+
+## Table of Contents
+- [Features](#features)
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Features
+
+- Create Vehicles: Supports creating Cars, Trucks, and Motorbikes with unique properties.
+
+- Perform Actions: Start, stop, accelerate, decelerate, and perform vehicle-specific actions like towing for trucks and wheelies for motorbikes.
+
+- Towing Capability: Trucks can tow other vehicles.
+
+- Unique VIN Generator: Each vehicle is assigned a unique Vehicle Identification Number (VIN).
+
+## Prerequisites
+
+- Node.js(version 12 or higher)
+- npm (Node Package Manager)
+
+## Installation
+
+1. Clone the repository:
+    ```bash
+    git clone https://github.com/xfigueroa/Vehicle-Builder
+
+2. Navigate to the project directory:
+    ```bash
+    cd Vehicle-Builder   
+
+3. Install the necessary dependencies using npm:
+    ```bash
+    npm install
+
+## Usage
+
+1. Start the CLI Application:
+    ```bash
+    npm start
+2. Follow the on-screen prompts to create or select a vehicle.
+
+## License
+
+This project is licensed under the [MIT](https://opensource.org/licenses/MIT) license.
+
+
+## Contributing
+
+If you would like to contribute, please copy the repository, create a feature branch, and submit a pull request. Contributions are always welcome!
+
+## Tests
+
+Link to video:
+
+https://drive.google.com/file/d/1rqQXnyIFS5QNSxQLoLWVxvR41pB8_j-U/view?usp=sharing
+
+## Questions
+If you have any questions, you can reach me at [xfigueroa28@gmail.com](mailto:xfigueroa28@gmail.com).
+You can also find me on GitHub at [xfigueroa](https://github.com/xfigueroa).
+
--- src/classes/Truck.ts
+++ src/classes/Truck.ts
@@ -33,6 +33,10 @@ class Truck extends Vehicle implements AbleToTow {
     towingCapacity: number,
   ){
 
+  // TODO: The constructor should call the constructor of the parent class, Vehicle
+  // TODO: The constructor should initialize the properties of the Truck class
+  // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
+    
   super();
   this.vin = vin;
   this.color = color;
@@ -43,12 +47,12 @@ class Truck extends Vehicle implements AbleToTow {
   this.topSpeed = topSpeed;
   this.wheels = wheels.length === 4 ? wheels : Array(4).fill(new Wheel());
   this.towingCapacity = towingCapacity;
-   // TODO: The constructor should call the constructor of the parent class, Vehicle
-   // TODO: The constructor should initialize the properties of the Truck class
-   // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
-   
   }
   // TODO: Implement the tow method from the AbleToTow interface
+  // TODO: Get the make an model of the vehicle if it exists
+  // TODO: Check if the vehicle's weight is less than or equal to the truck's towing capacity
+  // TODO: If it is, log that the vehicle is being towed
+  // TODO: If it is not, log that the vehicle is too heavy to be towed
   tow(vehicle: Truck | Motorbike | Car): void {
     const { make, model, weight } = vehicle;
     if (weight <= this.towingCapacity) {
@@ -56,34 +60,27 @@ class Truck extends Vehicle implements AbleToTow {
     } else {
       console.log(`The vehicle ${make} ${model} is too heavy to be towed.`);
     }
-    // TODO: Get the make an model of the vehicle if it exists
-    // TODO: Check if the vehicle's weight is less than or equal to the truck's towing capacity
-    // TODO: If it is, log that the vehicle is being towed
-    // TODO: If it is not, log that the vehicle is too heavy to be towed
   }
-    // TODO: Override the printDetails method from the Vehicle class
-    override printDetails(): void {
-    // TODO: The method should call the printDetails method of the parent class
+  // TODO: Override the printDetails method from the Vehicle class
+  override printDetails(): void {
+  // TODO: The method should call the printDetails method of the parent class
       
-      super.printDetails();
+  super.printDetails();
       
       
-    // TODO: The method should log the details of the Truck
-    // TODO: The details should include the VIN, make, model, year, weight, top speed, color, towing capacity, and wheels
-    console.log(`Truck Details:
-      VIN: ${this.vin}
-      Make: ${this.make}
-      Model: ${this.model}
-      Year: ${this.year}
-      Weight: ${this.weight}
-      Top Speed: ${this.topSpeed}
-      Color: ${this.color}
-      Towing Capacity: ${this.towingCapacity}
-      Wheels: ${this.wheels.length}`);
+  // TODO: The method should log the details of the Truck
+  // TODO: The details should include the VIN, make, model, year, weight, top speed, color, towing capacity, and wheels
+  console.log(`Truck Details:
+    VIN: ${this.vin}
+    Make: ${this.make}
+    Model: ${this.model}
+    Year: ${this.year}
+    Weight: ${this.weight}
+    Top Speed: ${this.topSpeed}
+    Color: ${this.color}
+    Towing Capacity: ${this.towingCapacity}
+    Wheels: ${this.wheels.length}`);
   }
 }
-
-
-
 // Export the Truck class as the default export
 export default Truck;
